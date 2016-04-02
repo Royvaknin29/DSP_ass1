e88425d487af63c9d4fe2f4d9464e0a6fdf4bb0d
roi_vaknin <roivaknin@post.bgu.ac.il>
2016-04-02 23:35:45 +0300

    ...

--- src/main/java/ec2_instances/Worker.java
+++ src/main/java/ec2_instances/Worker.java
@@ -3,18 +3,6 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Properties;
 
-import edu.stanford.nlp.ling.CoreAnnotations;
-import edu.stanford.nlp.ling.CoreAnnotations.NamedEntityTagAnnotation;
-import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
-import edu.stanford.nlp.ling.CoreAnnotations.TextAnnotation;
-import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
-import edu.stanford.nlp.ling.CoreLabel;
-import edu.stanford.nlp.pipeline.Annotation;
-import edu.stanford.nlp.pipeline.StanfordCoreNLP;
-import edu.stanford.nlp.rnn.RNNCoreAnnotations;
-import edu.stanford.nlp.sentiment.SentimentCoreAnnotations;
-import edu.stanford.nlp.trees.Tree;
-import edu.stanford.nlp.util.CoreMap;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
