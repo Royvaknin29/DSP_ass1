7f34aecb77ec7f1f608a119ffcf4ef459f2a4031
Aaron Vinestock <aaronvine@gmail.com>
2016-04-04 08:34:01 +0300

    update keys

--- src/main/java/local_application/RunLocalApplication.java
+++ src/main/java/local_application/RunLocalApplication.java
@@ -5,8 +5,8 @@ import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.BasicAWSCredentials;
 
 public class RunLocalApplication {
-	private static String accKey = "AKIAJ7NENWCNH4ZIBIQQ";
-	private static String secKey = "LWce1dJ65wK2ZCMYPTL+vnVLwBPMPh5fvNbxhnOC";
+	private static String accKey = "";
+	private static String secKey = "";
 
 	public static void main(String[] args) {
 		AWSCredentials credentials = setCredentialsFromArgs(accKey, secKey);
