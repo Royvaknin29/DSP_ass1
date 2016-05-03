439e415251df12166ff95121ba926a381f5b41fc
Roy <Roy@Roy-PC>
2016-05-02 17:13:01 -0700

    manager

--- src/main/java/ec2_instances/Manager.java
+++ src/main/java/ec2_instances/Manager.java
@@ -34,8 +34,8 @@ import ass1.amazon_utils.SQSservice;
 import local_application.TweetAnalysisOutput;
 
 public class Manager {
-	private static String accKey = "";
-	private static String secKey = "";
+	private static String accKey = "AKIAJIWZRPBTMYXF4K7Q";
+	private static String secKey = "Ad6zT9adXQTs7e1b3jx09+s0fbXi/5X9qoUbX2ra";
 	private static String localToManagerqueueName = "localAppToManager";
 	private static String jobsQueue = "jobsQueue";
 	private static String resultsQueue = "resultsQueue";
